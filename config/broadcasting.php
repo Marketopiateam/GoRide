c1fd095a766b2d5f4ac9bab28d0f1d27382d2b52
Mohamed Galal <mgalal8866@gmail.com>
2024-06-11 00:15:42 +0300

    modified:   config/broadcasting.php 	modified:   config/broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -43,8 +43,8 @@ return [
                 'debug' => true,
                 'useTLS' => true,
                 'curl_options' => [
-                    'CURLOPT_SSL_VERIFYHOST' => 0,
-                    'CURLOPT_SSL_VERIFYPEER' => 0,
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
 
                 ],
             ],
