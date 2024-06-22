41a85124d79bfa2e5e73c05f69bae393cc8f00e3
RichnessInc <omarkhaledjob2001@gmail.com>
2024-06-22 11:21:49 +0300

    commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -46,8 +46,9 @@ window.Echo = new Echo({
     wssPort: 6001
 
 });
-if (document.body.innerText.includes('ChatAppPage')) {
-    let element = document.getElementById('ChatAppPage');
+let element = document.getElementById('ChatAppPage');
+
+if (element != undefined && element != null) {
     console.log('trip-'+element.getAttribute('chat-id'));
     window.Echo.channel('trip-'+element.getAttribute('chat-id')).listen('.chat', (e) => {
         console.log(e);
