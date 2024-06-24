8208600a3f14a84b535bc6412ea30bdd91851185
Mohamed Galal <58759061+mgalal8866@users.noreply.github.com>
2024-06-24 13:23:18 +0300

    modified:   app/Http/Controllers/Api/V1/Admin/DriverApiController.php

--- app/Http/Controllers/Api/V1/Admin/DriverApiController.php
+++ app/Http/Controllers/Api/V1/Admin/DriverApiController.php
@@ -25,7 +25,7 @@ class DriverApiController extends Controller
 
         try {
             $user_id = Auth::user()->id;
-            
+
 
 
             if ($request->has('criminal_record_image')) {
@@ -60,10 +60,10 @@ class DriverApiController extends Controller
                 $car_driver_with_license_image = $this->saveImageAndThumbnail($request->car_driver_with_license_image, false, $user_id, 'DriverLicense');
             }
 
-            $DriverProfile =  DriverProfile::create(['user_id' => $user_id, 'birth_date' => $request->birth_date,  'id_number' => $request->id_number,   'criminal_record_image' => $criminal_record_image['image'],]);
+            $DriverProfile =  DriverProfile::create(['user_id' => $user_id, 'birth_date' => $request->birth_date,  'id_number' => $request->id_number,   'criminal_record_image' => $criminal_record_image['image'],'expiry_date' => $request->criminal_expiry_date]);
             $DriverIdentity = DriverIdentity::create(['id_number' => $request->id_number,    'front_identity_image' =>  $front_identity_image['image'], 'back_identity_image' => $back_identity_image['image'],    'expiry_date' => $request->expiry_date,    'driver_image_with_id' => $driver_image_with_id['image'],       'driver_profile_id' => $DriverProfile->id,]);
             $DriverCar =      DriverCar::create(['driver_profile_id' => $DriverProfile->id,    'car_brand_id' => $request->car_brand_id,    'car_model_id' => $request->car_model_id,    'color' => $request->color,    'release_year' => $request->release_year]);
-            $DriverLicense =  DriverLicense::create(['front_license_image' =>  $front_license_image['image'],   'back_license_image' => $back_license_image['image'], 'driver_with_license_image' => $driver_with_license_image['image'],    'expiry_date' => $request->expiry_date, 'driver_profile_id' => $DriverProfile->id]);
+            $DriverLicense =  DriverLicense::create(['front_license_image' =>  $front_license_image['image'],   'back_license_image' => $back_license_image['image'], 'driver_with_license_image' => $driver_with_license_image['image'],    'expiry_date' => $request->license_expiry_date, 'driver_profile_id' => $DriverProfile->id]);
             $DriverCarLicense =  DriverCarLicense::create(['front_license_image' => $car_front_license_image['image'],   'back_license_image' => $car_back_license_image['image'], 'driver_with_license_image' => $car_driver_with_license_image['image'],    'expiry_date' => $request->car_expiry_date,   'driver_profile_id' => $DriverProfile->id, 'driver_car_id' => $DriverCar->id]);
             DB::commit();
             return $this->apiResponseHandler(200, true, 'succes');
