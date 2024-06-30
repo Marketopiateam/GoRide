6b48e604f8032112e0bb38319015b73c1bdba21a
Mohamed Galal <58759061+mgalal8866@users.noreply.github.com>
2024-06-30 16:11:40 +0300

    modified:   app/Http/Controllers/Api/V1/Admin/OrderApiController.php

--- app/Http/Controllers/Api/V1/Admin/OrderApiController.php
+++ app/Http/Controllers/Api/V1/Admin/OrderApiController.php
@@ -95,9 +95,9 @@ class OrderApiController extends Controller
         $order->car_number       = $user->profile->car_licenses->car_number ?? '';
         $order->car_brand        = $user->profile->driver_cars->brand->title ?? '';
         $order->car_model        = $user->profile->driver_cars->model->title ?? '';
-        
+
         TripOffers::dispatch($order);
-        return Resp(new OrderWithDriverResource($order), 'success');
+        return Resp(new OrderWithDriverResource($order->toarray()), 'success');
     }
 
     public function endorder(Request $request, Order $order)
